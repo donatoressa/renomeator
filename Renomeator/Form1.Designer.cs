a44c491a0a69f62b671cb6c9ad2d7ac1e7276dc8
Donato Ressa <donatoressa@gmail.com>
2016-08-11 10:04:16 -0300

    push inicial

---
+++ Renomeator/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Renomeator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkCedilha = new System.Windows.Forms.CheckBox();
+            this.checkAcomTio = new System.Windows.Forms.CheckBox();
+            this.checkOcomTio = new System.Windows.Forms.CheckBox();
+            this.checkAcomAcento = new System.Windows.Forms.CheckBox();
+            this.checkEcomAcento = new System.Windows.Forms.CheckBox();
+            this.checkIcomAcento = new System.Windows.Forms.CheckBox();
+            this.checkAcomCrase = new System.Windows.Forms.CheckBox();
+            this.checkEcomCircunflexo = new System.Windows.Forms.CheckBox();
+            this.checkOcomCircunflexo = new System.Windows.Forms.CheckBox();
+            this.checkUcomTrema = new System.Windows.Forms.CheckBox();
+            this.groupBoxOpcoes = new System.Windows.Forms.GroupBox();
+            this.checkAll = new System.Windows.Forms.CheckBox();
+            this.checkHifen = new System.Windows.Forms.CheckBox();
+            this.checkUcomAcento = new System.Windows.Forms.CheckBox();
+            this.checkOcomAcento = new System.Windows.Forms.CheckBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRenomear = new System.Windows.Forms.Button();
+            this.groupBoxOpcoes.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkCedilha
+            // 
+            this.checkCedilha.AutoSize = true;
+            this.checkCedilha.Location = new System.Drawing.Point(16, 96);
+            this.checkCedilha.Name = "checkCedilha";
+            this.checkCedilha.Size = new System.Drawing.Size(55, 17);
+            this.checkCedilha.TabIndex = 0;
+            this.checkCedilha.Text = "Ç -> C";
+            this.checkCedilha.UseVisualStyleBackColor = true;
+            // 
+            // checkAcomTio
+            // 
+            this.checkAcomTio.AutoSize = true;
+            this.checkAcomTio.Location = new System.Drawing.Point(16, 50);
+            this.checkAcomTio.Name = "checkAcomTio";
+            this.checkAcomTio.Size = new System.Drawing.Size(55, 17);
+            this.checkAcomTio.TabIndex = 1;
+            this.checkAcomTio.Text = "Ã -> A";
+            this.checkAcomTio.UseVisualStyleBackColor = true;
+            // 
+            // checkOcomTio
+            // 
+            this.checkOcomTio.AutoSize = true;
+            this.checkOcomTio.Location = new System.Drawing.Point(16, 73);
+            this.checkOcomTio.Name = "checkOcomTio";
+            this.checkOcomTio.Size = new System.Drawing.Size(57, 17);
+            this.checkOcomTio.TabIndex = 2;
+            this.checkOcomTio.Text = "Õ -> O";
+            this.checkOcomTio.UseVisualStyleBackColor = true;
+            // 
+            // checkAcomAcento
+            // 
+            this.checkAcomAcento.AutoSize = true;
+            this.checkAcomAcento.Location = new System.Drawing.Point(96, 50);
+            this.checkAcomAcento.Name = "checkAcomAcento";
+            this.checkAcomAcento.Size = new System.Drawing.Size(55, 17);
+            this.checkAcomAcento.TabIndex = 3;
+            this.checkAcomAcento.Text = "Á -> A";
+            this.checkAcomAcento.UseVisualStyleBackColor = true;
+            // 
+            // checkEcomAcento
+            // 
+            this.checkEcomAcento.AutoSize = true;
+            this.checkEcomAcento.Location = new System.Drawing.Point(96, 73);
+            this.checkEcomAcento.Name = "checkEcomAcento";
+            this.checkEcomAcento.Size = new System.Drawing.Size(55, 17);
+            this.checkEcomAcento.TabIndex = 4;
+            this.checkEcomAcento.Text = "É -> E";
+            this.checkEcomAcento.UseVisualStyleBackColor = true;
+            // 
+            // checkIcomAcento
+            // 
+            this.checkIcomAcento.AutoSize = true;
+            this.checkIcomAcento.Location = new System.Drawing.Point(96, 96);
+            this.checkIcomAcento.Name = "checkIcomAcento";
+            this.checkIcomAcento.Size = new System.Drawing.Size(47, 17);
+            this.checkIcomAcento.TabIndex = 5;
+            this.checkIcomAcento.Text = "Í -> I";
+            this.checkIcomAcento.UseVisualStyleBackColor = true;
+            // 
+            // checkAcomCrase
+            // 
+            this.checkAcomCrase.AutoSize = true;
+            this.checkAcomCrase.Location = new System.Drawing.Point(173, 74);
+            this.checkAcomCrase.Name = "checkAcomCrase";
+            this.checkAcomCrase.Size = new System.Drawing.Size(55, 17);
+            this.checkAcomCrase.TabIndex = 7;
+            this.checkAcomCrase.Text = "À -> A";
+            this.checkAcomCrase.UseVisualStyleBackColor = true;
+            // 
+            // checkEcomCircunflexo
+            // 
+            this.checkEcomCircunflexo.AutoSize = true;
+            this.checkEcomCircunflexo.Location = new System.Drawing.Point(173, 96);
+            this.checkEcomCircunflexo.Name = "checkEcomCircunflexo";
+            this.checkEcomCircunflexo.Size = new System.Drawing.Size(55, 17);
+            this.checkEcomCircunflexo.TabIndex = 8;
+            this.checkEcomCircunflexo.Text = "Ê -> E";
+            this.checkEcomCircunflexo.UseVisualStyleBackColor = true;
+            // 
+            // checkOcomCircunflexo
+            // 
+            this.checkOcomCircunflexo.AutoSize = true;
+            this.checkOcomCircunflexo.Location = new System.Drawing.Point(173, 119);
+            this.checkOcomCircunflexo.Name = "checkOcomCircunflexo";
+            this.checkOcomCircunflexo.Size = new System.Drawing.Size(57, 17);
+            this.checkOcomCircunflexo.TabIndex = 9;
+            this.checkOcomCircunflexo.Text = "Ô -> O";
+            this.checkOcomCircunflexo.UseVisualStyleBackColor = true;
+            // 
+            // checkUcomTrema
+            // 
+            this.checkUcomTrema.AutoSize = true;
+            this.checkUcomTrema.Location = new System.Drawing.Point(16, 119);
+            this.checkUcomTrema.Name = "checkUcomTrema";
+            this.checkUcomTrema.Size = new System.Drawing.Size(57, 17);
+            this.checkUcomTrema.TabIndex = 10;
+            this.checkUcomTrema.Text = "Ü -> U";
+            this.checkUcomTrema.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxOpcoes
+            // 
+            this.groupBoxOpcoes.Controls.Add(this.checkAll);
+            this.groupBoxOpcoes.Controls.Add(this.checkHifen);
+            this.groupBoxOpcoes.Controls.Add(this.checkUcomAcento);
+            this.groupBoxOpcoes.Controls.Add(this.checkOcomAcento);
+            this.groupBoxOpcoes.Controls.Add(this.checkAcomTio);
+            this.groupBoxOpcoes.Controls.Add(this.checkOcomCircunflexo);
+            this.groupBoxOpcoes.Controls.Add(this.checkUcomTrema);
+            this.groupBoxOpcoes.Controls.Add(this.checkEcomCircunflexo);
+            this.groupBoxOpcoes.Controls.Add(this.checkOcomTio);
+            this.groupBoxOpcoes.Controls.Add(this.checkAcomCrase);
+            this.groupBoxOpcoes.Controls.Add(this.checkCedilha);
+            this.groupBoxOpcoes.Controls.Add(this.checkAcomAcento);
+            this.groupBoxOpcoes.Controls.Add(this.checkIcomAcento);
+            this.groupBoxOpcoes.Controls.Add(this.checkEcomAcento);
+            this.groupBoxOpcoes.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxOpcoes.Name = "groupBoxOpcoes";
+            this.groupBoxOpcoes.Size = new System.Drawing.Size(268, 176);
+            this.groupBoxOpcoes.TabIndex = 11;
+            this.groupBoxOpcoes.TabStop = false;
+            this.groupBoxOpcoes.Text = "Selecione as opções desejadas:";
+            // 
+            // checkAll
+            // 
+            this.checkAll.AutoSize = true;
+            this.checkAll.Location = new System.Drawing.Point(16, 20);
+            this.checkAll.Name = "checkAll";
+            this.checkAll.Size = new System.Drawing.Size(56, 17);
+            this.checkAll.TabIndex = 14;
+            this.checkAll.Text = "Todos";
+            this.checkAll.UseVisualStyleBackColor = true;
+            this.checkAll.CheckedChanged += new System.EventHandler(this.checkAll_CheckedChanged);
+            // 
+            // checkHifen
+            // 
+            this.checkHifen.AutoSize = true;
+            this.checkHifen.Location = new System.Drawing.Point(173, 141);
+            this.checkHifen.Name = "checkHifen";
+            this.checkHifen.Size = new System.Drawing.Size(50, 17);
+            this.checkHifen.TabIndex = 13;
+            this.checkHifen.Text = "- -> _";
+            this.checkHifen.UseVisualStyleBackColor = true;
+            // 
+            // checkUcomAcento
+            // 
+            this.checkUcomAcento.AutoSize = true;
+            this.checkUcomAcento.Location = new System.Drawing.Point(173, 50);
+            this.checkUcomAcento.Name = "checkUcomAcento";
+            this.checkUcomAcento.Size = new System.Drawing.Size(57, 17);
+            this.checkUcomAcento.TabIndex = 12;
+            this.checkUcomAcento.Text = "Ú -> U";
+            this.checkUcomAcento.UseVisualStyleBackColor = true;
+            // 
+            // checkOcomAcento
+            // 
+            this.checkOcomAcento.AutoSize = true;
+            this.checkOcomAcento.Location = new System.Drawing.Point(96, 119);
+            this.checkOcomAcento.Name = "checkOcomAcento";
+            this.checkOcomAcento.Size = new System.Drawing.Size(57, 17);
+            this.checkOcomAcento.TabIndex = 11;
+            this.checkOcomAcento.Text = "Ó -> O";
+            this.checkOcomAcento.UseVisualStyleBackColor = true;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(16, 33);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(182, 20);
+            this.tbPath.TabIndex = 12;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(204, 33);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(58, 23);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.tbPath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 62);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selecione o diretório cujos arquivos serão renomeados:";
+            // 
+            // btnRenomear
+            // 
+            this.btnRenomear.Location = new System.Drawing.Point(207, 262);
+            this.btnRenomear.Name = "btnRenomear";
+            this.btnRenomear.Size = new System.Drawing.Size(67, 23);
+            this.btnRenomear.TabIndex = 15;
+            this.btnRenomear.Text = "Renomear";
+            this.btnRenomear.UseVisualStyleBackColor = true;
+            this.btnRenomear.Click += new System.EventHandler(this.btnRenomear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 297);
+            this.Controls.Add(this.btnRenomear);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxOpcoes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Renomeator";
+            this.groupBoxOpcoes.ResumeLayout(false);
+            this.groupBoxOpcoes.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkCedilha;
+        private System.Windows.Forms.CheckBox checkAcomTio;
+        private System.Windows.Forms.CheckBox checkOcomTio;
+        private System.Windows.Forms.CheckBox checkAcomAcento;
+        private System.Windows.Forms.CheckBox checkEcomAcento;
+        private System.Windows.Forms.CheckBox checkIcomAcento;
+        private System.Windows.Forms.CheckBox checkAcomCrase;
+        private System.Windows.Forms.CheckBox checkEcomCircunflexo;
+        private System.Windows.Forms.CheckBox checkOcomCircunflexo;
+        private System.Windows.Forms.CheckBox checkUcomTrema;
+        private System.Windows.Forms.GroupBox groupBoxOpcoes;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRenomear;
+        private System.Windows.Forms.CheckBox checkUcomAcento;
+        private System.Windows.Forms.CheckBox checkOcomAcento;
+        private System.Windows.Forms.CheckBox checkHifen;
+        private System.Windows.Forms.CheckBox checkAll;
+    }
+}
+
---
+++ Renomeator/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Renomeator
+{
+    public partial class Form1 : Form
+    {
+        private int contador;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            tbPath.Text = fbd.SelectedPath;
+        }
+
+        private void btnRenomear_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+
+            try
+            {
+                if (checkAcomAcento.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Á", "A");
+                }
+                if (checkEcomAcento.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "É", "E");
+                }
+                if (checkIcomAcento.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Í", "I");
+                }
+                if (checkOcomAcento.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ó", "O");
+                }
+                if (checkUcomAcento.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ú", "U");
+                }
+                if (checkCedilha.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ç", "C");
+                }
+                if (checkAcomTio.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ã", "A");
+                }
+                if (checkOcomTio.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Õ", "O");
+                }
+                if (checkEcomCircunflexo.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ê", "E");
+                }
+                if (checkOcomCircunflexo.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ô", "O");
+                }
+                if (checkAcomCrase.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "À", "A");
+                }
+                if (checkUcomTrema.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "Ü", "U");
+                }
+                if (checkHifen.Checked)
+                {
+                    RenomearArquivos(tbPath.Text, "-", "_");
+                }
+
+                if (contador > 0)
+                {
+                    MessageBox.Show(contador + " arquivos renomeados com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    MessageBox.Show("Não existem arquivos a serem renomeados.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao renomear arquivos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void RenomearArquivos(string path, string oldChar_, string newChar_)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            FileInfo[] arquivos = dirInfo.GetFiles();
+
+            foreach (FileInfo f in arquivos)
+            {
+                if(f.Name.Contains(oldChar_) || f.Name.Contains(oldChar_.ToLower()))
+                {
+                    string nomeInicial = f.Name;
+                    string nomeFinal = string.Empty;
+                    nomeFinal = f.Name.Replace(oldChar_, newChar_).Replace(oldChar_.ToString().ToLower(), newChar_.ToString().ToLower());
+                    Directory.Move(path + "\\" + nomeInicial, path + "\\" + nomeFinal);
+                    contador++;
+                }
+            }
+        }
+
+        private void checkAll_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkAll.Checked)
+            {
+                foreach (Control c in groupBoxOpcoes.Controls)
+                {
+                    if (c is CheckBox)
+                    {
+                        ((CheckBox)c).Checked = true;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Control c in groupBoxOpcoes.Controls)
+                {
+                    if (c is CheckBox)
+                    {
+                        ((CheckBox)c).Checked = false;
+                    }
+                }
+            }
+        }
+    }
+}
